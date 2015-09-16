6aeda7790d46f875b4e8880dd5e999e27b169030
Monchai Onlamai <monchai.onlamai@lex247.com>
2015-09-16 10:21:36 +0700

    Add IsValid function

--- CryptoDemo/StringUtility.cs
+++ CryptoDemo/StringUtility.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace CryptoDemo {
   public class StringUtility {
+    public static bool IsValid(string input) {
+      if (!string.IsNullOrEmpty(input))
+        return false;
+
+      return true;
+    }
   }
 }
